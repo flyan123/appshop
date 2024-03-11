5dd0591065b37344eaa419366b34ad7246bad3ed
flyan123 <2495365901@qq.com>
2024-03-11 23:14:29 +0800

    add change

---
+++ .eslintrc.cjs
@@ -0,0 +1,37 @@
+module.exports = {
+    // 运行环境
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    // 规则继承
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:vue/vue3-essential"
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "parser": "@typescript-eslint/parser",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint",
+        "vue"
+    ],
+    "rules": {
+    }
+}
