92fb6c40a1ed04d4a9fe49d800bce502a55995d4
flyan123 <2495365901@qq.com>
2024-03-17 20:36:25 +0800

    add 环境变量配置

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <div>
     <el-button type="primary" size="default" :icon="Plus">主要按钮</el-button>
+    <Text/>
   </div>
 </template>
 
 <script setup lang="ts">
 // 引入图标组件
-import {Plus} from '@element-plus/icons-vue'
+import { Plus } from '@element-plus/icons-vue'
+import Text from '@/components/text.vue'
 </script>
 
 <style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,18 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from '@/App.vue'
 // 引入element-plus
 import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css' 
+import 'element-plus/dist/index.css'
 // 配置element-plus国际化
 // @ts-ignore
 import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 // 获取应用实例对象
 const app = createApp(App)
-// 安装element-plus插件 
+// 安装element-plus插件
 app.use(ElementPlus, {
-    locale: zhCn
+  locale: zhCn,
 })
+console.log(import.meta.env)
 // 将应用挂载到挂载点上
 app.mount('#app')
 // createApp(App).mount('#app')
-
-
