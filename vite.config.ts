5cf0792139a507fcbbb985a598f82b4528eaa817
flyan123 <2495365901@qq.com>
2024-03-18 23:06:34 +0800

    add svg1

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,12 @@ import path from 'path'
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(),
+  plugins: [
+    vue(),
     createSvgIconsPlugin({
       iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
       symbolId: 'icon-[dir]-[name]',
-    })
+    }),
   ],
   resolve: {
     alias: {
